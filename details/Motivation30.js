c47e98495b1c78c818ad8734d6a7dbe17de56e65
James Berry <james.berry@yahoo.com>
2016-12-20 12:23:18 -0800

    Revert "Revert "Starting work on #2""
    This reverts commit c3f4d7164fe0099fef426f83f96cd2bc31b2639b.

---
+++ details/Motivation30.js
@@ -0,0 +1,27 @@
+/**
+ * params - Object of the form { Phases: {}, TextEvents: {} }
+ */
+var 	 Params				= {};
+function Block()			{}
+function setParams(params)	{ return Params = params; }
+function getParams()		{ return Params; }
+
+/**
+ *
+ */
+Block.prototype.init = function (params)	{ setParams(params); }
+
+/**
+ *
+ */
+Block.prototype.process = function () {
+	Context = getParams();
+	
+	if (Context.Line.Value === true) {
+		Context.TextEvents.setMotivationAllOn('Motivation30', Context.Phase, Context.Classnum);
+	}
+	Context.TextEvents.deleteBlock(Context.Phase, Context.Classnum, Context.Blocknum);
+	return false;	//return this.render(duration, duration_off, power, power_off, power_high, cadence, cadence_off, repeat, flatroad, textevents);
+}
+
+module.exports = Block;
